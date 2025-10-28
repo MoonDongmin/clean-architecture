6bb6fcb338d1332553ce45a557d40ef7e1a63c79
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-10-28 10:27:01 +0900

    Error 수정

--- src/common/decorators/web-adapter.decorator.ts
+++ src/common/decorators/web-adapter.decorator.ts
@@ -3,4 +3,6 @@ import { Controller } from "@nestjs/common";
  * @WebAdapter Decorator
  * - Web 어댑터를 명시적으로 표시
  */
-export const WebAdapter = (path?: string) => Controller(path);
+export const WebAdapter = (path?: string) => {
+  return path ? Controller(path) : Controller();
+};
